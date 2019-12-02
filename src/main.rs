4726c7ae32e52dc4c32bf4668e374ca6e81e48c2
Packy White <pwhite@lyft.com>
2019-12-02 13:34:22 -0800

    day1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use std::fs;
 use std::env::args;
+use std::fs;
 use std::io::{BufRead, BufReader, Cursor};
+
+fn fuel_per_mass(x: i64) -> i64 {
+    (x / 3) - 2
+}
+
+fn total_fuel_per_mass(x: i64) -> i64 {
+    let mut total = 0;
+    let mut mass = fuel_per_mass(x);
+    while mass > 0 {
+        total += mass;
+        mass = fuel_per_mass(mass);
+    }
+    total
+}
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -8,15 +22,30 @@ fn main() {
         eprintln!("I expect exactly one argument - a file");
         std::process::exit(1);
     }
-    let total: u64 = fs::read(&args[1])
+    let total: i64 = fs::read(&args[1])
         .map(Cursor::new)
         .map(BufReader::new)
         .expect("Unable to read the file")
         .lines()
-        .filter_map(|l| l.ok())
-        .filter_map(|line| line.parse::<u64>().ok())
-        .map(|mass| (mass / 3) - 2)
+        .filter_map(std::result::Result::ok)
+        .filter_map(|line| line.parse::<i64>().ok())
+        .map(total_fuel_per_mass)
         .sum();
-    
+
     println!("{}", total);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_samples() {
+        assert_eq!(fuel_per_mass(14), 2);
+    }
+
+    #[test]
+    fn test_totals_samples() {
+        assert_eq!(total_fuel_per_mass(14), 2);
+        assert_eq!(total_fuel_per_mass(1969), 966);
+    }
 }
